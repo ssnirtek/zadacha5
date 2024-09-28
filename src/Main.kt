5b84148824a4f20deef4d53c517758a5c74e7f9a
ssnirtek <ekaterinasysoeva3085@gmail.com>
2024-09-28 11:54:49 +0300

    лдло

--- src/Main.kt
+++ src/Main.kt
@@ -3,18 +3,14 @@ import kotlin.math.abs
 fun main() {
     println("С помощью данной программ пользователь, введя целое число n и основание степени x может узнать,\n существует ли целочисленный показатель степени y для которого выполняется равенство x (в степени) y = n.\n")
     while (true) {
-        println("Введите целое число n и основание степени x через пробел (или введите 'exit' для выхода):")
+        println("Введите целое число n и основание степени x через пробел:")
 
         val input = readLine()?.trim()
-        if (input.equals("exit", ignoreCase = true)) {
-            println("Программа завершена.")
-            break
-        }
 
         val sections = input?.split(" ")
 
         if (sections == null || sections.size != 2) {
-            println("Ошибка: введите два числа через пробел. Пример: 11 8")
+            println("Ошибка: введите два числа через пробел. Пример: 9 3")
             println()
             continue
         }
@@ -22,16 +18,16 @@ fun main() {
         try {
             val n = sections[0].toInt()
             val x = sections[1].toInt()
-
+                //для нуля
             if (x == 0) {
                 if (n == 0) {
-                    println("Любое число в степени 0 даёт 1. Таким образом, 0^y = 0 при y >= 1.")
+                    println("Любое число в степени 0 даёт 1. Следовательно 0^y = 0 при y >= 1.")
                 } else {
                     println("Целочисленный показатель не существует для x = 0, если n != 0.")
                 }
                 continue
             }
-
+//для 1
             if (x == 1) {
                 if (n == 1) {
                     println("Целочисленный показатель (y) может быть любым значением для x = 1 и n = 1.")
@@ -42,7 +38,7 @@ fun main() {
                 continue
             }
 
-            var y = 0
+            var y = 0 //показатель степени с нуля начиная
             var currentPower = 1
             val absN = abs(n)
 
@@ -64,7 +60,7 @@ fun main() {
         } catch (e: NumberFormatException) {
             println("Ошибка: введены некорректные числа. Попробуйте еще раз")
         }
-
+break
         println()
     }
 }
